387021c9313d873bb98b3f0e42a638466f87216a
KaisarOrange <alif.ayodya@gmail.com>
2023-06-11 13:34:23 +0700

    fix

--- dist/index.js
+++ dist/index.js
@@ -17,33 +17,36 @@ const pgSimpleStore = require('connect-pg-simple')(session);
 dotenv_1.default.config();
 require('./middleware/passportAuth');
 const app = (0, express_1.default)();
-const port = process.env.PORT || 3000;
-const corsConfig = {
-    origin: true,
-    credentials: true,
-};
-app.use(cors(corsConfig));
-app.use(express_1.default.json());
-app.use(cookieParser());
-app.use(express_1.default.static(path.join(__dirname, 'public')));
-// app.use(
-//   session({
-//     store: new pgSimpleStore({
-//       pool: db,
-//     }),
-//     secret: process.env.SECRET,
-//     saveUninitialized: false,
-//     resave: false,
-//     cookie: { maxAge: 1000 * 60 * 60 * 24 * 1 }, // 30 days
-//     // Insert express-session options here
-//   })
-// );
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(passport.authenticate('session'));
-app.use('/auth', auth);
-app.use('/order', get);
-app.use('/finance', finance);
+const port = process.env.PORT;
+// const corsConfig = {
+//   origin: true,
+//   credentials: true,
+// };
+// app.use(cors(corsConfig));
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+// // app.use(
+// //   session({
+// //     store: new pgSimpleStore({
+// //       pool: db,
+// //     }),
+// //     secret: process.env.SECRET,
+// //     saveUninitialized: false,
+// //     resave: false,
+// //     cookie: { maxAge: 1000 * 60 * 60 * 24 * 1 }, // 30 days
+// //     // Insert express-session options here
+// //   })
+// // );
+app.get('/', function (req, res) {
+    res.send({ title: 'GeeksforGeeks' });
+});
+// app.use(passport.initialize());
+// app.use(passport.session());
+// app.use(passport.authenticate('session'));
+// app.use('/auth', auth);
+// app.use('/order', get);
+// app.use('/finance', finance);
 //app.use(express.urlencoded());
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
