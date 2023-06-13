770579296926b020937b6dfa314d830149042bf2
KaisarOrange <alif.ayodya@gmail.com>
2023-06-13 11:38:51 +0700

    fix routes

--- api/order/order-routes.ts
+++ api/order/order-routes.ts
@@ -14,16 +14,16 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-router.get('/getOrder/:done/:page', authCheck, getCustomer);
-router.get('/detail/:id', authCheck, getDetail);
-router.get('/totalrows/:done', authCheck, getTotalRows);
+router.get('/getOrder/:done/:page', getCustomer);
+router.get('/detail/:id', getDetail);
+router.get('/totalrows/:done', getTotalRows);
 
 router.get('/test', (req: any, res: any) => {
   res.status(200).json({ data: req.user });
 });
 
-router.put('/finishOrder', authCheck, finishOrder);
-router.put('/revokeFinishOrder', authCheck, revokeFinishOrder);
+router.put('/finishOrder', finishOrder);
+router.put('/revokeFinishOrder', revokeFinishOrder);
 
 router.post('/pesan', insertOrder);
 
