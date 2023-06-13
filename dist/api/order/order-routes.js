60ae08b542c729654d67a83b1a575d7b90431aac
KaisarOrange <alif.ayodya@gmail.com>
2023-06-13 09:09:20 +0700

    remove ath

--- dist/api/order/order-routes.js
+++ dist/api/order/order-routes.js
@@ -1,18 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const order_controller_1 = require("./order-controller");
-const authCheck_1 = require("../../middleware/authCheck");
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-router.get('/getOrder/:done/:page', authCheck_1.authCheck, order_controller_1.getCustomer);
-router.get('/detail/:id', authCheck_1.authCheck, order_controller_1.getDetail);
-router.get('/totalrows/:done', authCheck_1.authCheck, order_controller_1.getTotalRows);
+router.get('/getOrder/:done/:page', order_controller_1.getCustomer);
+router.get('/detail/:id', order_controller_1.getDetail);
+router.get('/totalrows/:done', order_controller_1.getTotalRows);
 router.get('/test', (req, res) => {
     res.send(req.user);
 });
-router.put('/finishOrder', authCheck_1.authCheck, order_controller_1.finishOrder);
-router.put('/revokeFinishOrder', authCheck_1.authCheck, order_controller_1.revokeFinishOrder);
+router.put('/finishOrder', order_controller_1.finishOrder);
+router.put('/revokeFinishOrder', order_controller_1.revokeFinishOrder);
 router.post('/pesan', order_controller_1.insertOrder);
 router.delete('/delete/:id', order_controller_1.deleteCustomer);
 module.exports = router;
